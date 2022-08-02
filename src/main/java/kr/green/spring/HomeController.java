0107a50e3880ac595220ac829dec278d7f9f374b
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-08-02 12:03:57 +0900

    Spring 2일차 : 타일즈 최적화_템플릿 설정하기

--- src/main/java/kr/green/spring/HomeController.java
+++ src/main/java/kr/green/spring/HomeController.java
@@ -4,13 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
 	@Controller
 	public class HomeController {
-		
-		@RequestMapping(value= {"/main/home","/home.do"})
+
+		@RequestMapping(value= {"/"})
 		public ModelAndView openTilesView(ModelAndView mv) throws Exception{
 	    mv.setViewName("/main/home");
 	    mv.addObject("setHeader", "타일즈");
